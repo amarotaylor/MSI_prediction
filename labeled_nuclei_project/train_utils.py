825041abfd35c994fec5f2d1566fae4d7c7f3004
chsher <sxchao@pt-v100-cu92.c.mgh-getzlab-czi-scrnaseq.internal>
2019-04-11 16:44:57 +0000

    working rationales model

--- labeled_nuclei_project/train_utils.py
+++ labeled_nuclei_project/train_utils.py
@@ -197,3 +197,115 @@ def rationales_validation_loop(e, valid_loader, gen, enc, pool_fn, num_samples, 
     print('Epoch: {0}, Val Loss: {1:0.4f}, Val Acc: {2:0.4f}'.format(e, total_loss, acc))
 
     return total_loss
+
+
+def sample_gumbel(shape, eps=1e-20): 
+    """Sample from Gumbel(0, 1)"""
+    U = torch.rand(shape,dtype=torch.float32,device='cuda')
+    return -torch.log(-torch.log(U + eps) + eps)
+
+
+def gumbel_softmax_sample(logits, temperature): 
+    """ Draw a sample from the Gumbel-Softmax distribution"""
+    y = logits + sample_gumbel(logits.shape)
+    return F.softmax( y / temperature,dim=1)
+
+
+def gumbel_softmax(logits, temperature, hard=False):
+    """Sample from the Gumbel-Softmax distribution and optionally discretize.
+  Args:
+    logits: [batch_size, n_class] unnormalized log-probs
+    temperature: non-negative scalar
+    hard: if True, take argmax, but differentiate w.r.t. soft sample y
+  Returns:
+    [batch_size, n_class] sample from the Gumbel-Softmax distribution.
+    If hard=True, then the returned sample will be one-hot, otherwise it will
+    be a probabilitiy distribution that sums to 1 across classes
+  """
+    y = gumbel_softmax_sample(logits, temperature)
+    if hard:
+        y = torch.argmax(logits,dim=1)
+    return y
+
+
+def rationales_training_loop_GS(e, train_loader, gen, enc, pool_fn, lamb1, lamb2, xent, learning_rate, optimizer):
+    gen.train()
+    enc.train()
+    
+    rat_tiles = 0
+    total_tiles = 0
+    total_xeloss = 0
+    total_omega = 0
+    
+    for slide,label in train_loader:
+        slide,label = slide.squeeze(0).cuda(),label.cuda()
+        
+        # generate tile rationales
+        preds = gen(slide)
+        logits = lsm(preds).squeeze(0)
+        sample = gumbel_softmax(logits, temperature=10.0)
+        rationale = slide.view(slide.shape[1],slide.shape[2],slide.shape[3],-1) * sample[:,1]
+        rationale = rationale.view(-1,slide.shape[1],slide.shape[2],slide.shape[3])
+        
+        # predict class based on rationales
+        output = enc(rationale)
+        pool = pool_fn(output)
+        y_hat = enc.classification_layer(pool)
+        
+        # compute loss and regularization term
+        znorm = torch.sum(sample[:,1])
+        zdist = torch.sum(torch.abs(sample[:-1,1] - sample[1:,1]))
+        omega = ((lamb1 * znorm) + (lamb2 * zdist)) / sample.shape[0]
+        xeloss = xent(y_hat.unsqueeze(0), label)
+        loss = xeloss + omega
+        loss.backward()
+        optimizer.step()
+        
+        optimizer.zero_grad()
+        total_xeloss += xeloss.detach().cpu().numpy()
+        total_omega += omega.detach().cpu().numpy() 
+        
+        rat_tiles += znorm
+        total_tiles += float(sample.shape[0])
+        
+    frac_tiles = rat_tiles / total_tiles
+    print('Epoch: {0}, Train Loss: {1:04f}, Train Omega: {2:0.4f}, Fraction of Tiles: {3:0.4f}'.format(e, total_xeloss, 
+                                                                                                      total_omega, frac_tiles))
+    
+
+def rationales_validation_loop_GS(e, valid_loader, gen, enc, pool_fn, xent):
+    gen.eval()
+    enc.eval()
+
+    rat_tiles = 0
+    total_tiles = 1
+    total_loss = 0
+    labels = []
+    preds = []
+
+    for slide,label in valid_loader:
+        slide, label = slide.squeeze(0).cuda(), label.cuda()
+
+        prez = gen(slide)
+        z = torch.argmax(prez, dim=2).squeeze(0)
+        rationale = slide[z==1,:,:,:]
+        znorm = torch.sum(z.float())
+
+        if znorm > 0:
+            output = enc(rationale)
+            pool = pool_fn(output)
+            y_hat = enc.classification_layer(pool)
+
+            loss = xent(y_hat.unsqueeze(0), label)
+            total_loss += loss.detach().cpu().numpy()
+
+            rat_tiles += znorm
+            total_tiles += float(z.shape[0])
+
+            labels.extend(label.float().cpu().numpy())
+            preds.append(torch.argmax(y_hat).float().detach().cpu().numpy())
+
+    acc = np.mean(np.array(labels) == np.array(preds))
+    frac_tiles = rat_tiles / total_tiles
+    print('Epoch: {0}, Val Loss: {1:0.4f}, Val Acc: {2:0.4f}, Fraction of Tiles: {3:0.4f}'.format(e, total_loss, acc,
+                                                                                                  frac_tiles))
